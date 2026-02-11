d56c119d405c8bf2f9021c36e9ef21cce1b984b0
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-02-10 17:10:40 -0800

    added all of the steps with a reflection at the end.

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DisplayWelcome();
+
+            string name = "Alex";
+            int age = 20;
+            DisplayUserInfo(name, age);
+
+            int sum = CalculateSum(7, 8);
+            Console.WriteLine("The sum is: " + sum);
+
+            int mainOnlyNumber = 55;
+
+            if (true)
+            {
+                int insideIf = 6;
+                Console.WriteLine(insideIf);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            static void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to Module 5!");
+            }
+
+            static void DisplayUserInfo(string name, int age)
+            {
+                Console.WriteLine("Name: " + name + ", Age: " + age);
+            }
+
+            static int CalculateSum(int a, int b)
+            {
+                return a + b;
+            }
+            /* Method variables are not accessible in Main because they are only local to that method
+             * 
+             * Block variables do not exist outside {} so it won't share the methods inside variables 
+             * it is only to pass the values using the parameters or return values
+             * 
+             * Why scope improves program safety is because it helps prevents accidental changes
+             * and with that you can reuse names safely
+             */
         }
     }
 }
